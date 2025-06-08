30f143c141b62a566b1321b421dd9c6eb157c5fc
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-06-08 13:37:18 -0400

    added todo model

--- Todo/obj/Debug/net9.0/Todo.AssemblyInfo.cs
+++ Todo/obj/Debug/net9.0/Todo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceb26e83eadc34442947c172392defa1eebdb9cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27d48bacf4ff2304b60b3268c8c8310a8070776c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
