db01dfca09f9bbc82e25e4c8461ef90e4920dcfa
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-06-08 13:51:14 -0400

    added todo context

--- Todo/Program.cs
+++ Todo/Program.cs
@@ -1,5 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Todo.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoContext>(options =>
+        options.UseSqlite(builder.Configuration.GetConnectionString("TodoContext")));
+
+
+        
 var app = builder.Build();
+
+
+
 
 app.MapGet("/", () => "Hello World!");
 
